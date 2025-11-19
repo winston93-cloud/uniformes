90957ae70cb0cc73fa39f0c9355f3d5f16d43a94
winston <sistemas.desarrollo@winston93.edu.mx>
2025-11-19 08:21:56 -0600

    Fix: Cambiar redirect de la página raíz para Vercel

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,5 @@
-'use client';
-
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
+import { redirect } from 'next/navigation';
 
 export default function Home() {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.push('/dashboard');
-  }, [router]);
-
-  return (
-    <div className="loading">
-      <div className="spinner"></div>
-    </div>
-  );
+  redirect('/dashboard');
 }
